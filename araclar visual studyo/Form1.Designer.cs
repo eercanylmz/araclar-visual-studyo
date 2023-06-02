5a901c71c8e42d79873999277dea7470493d1663
yilma <yilma@ERCANMONSTER>
2023-06-02 20:16:34 +0300

    Proje dosyası ekle.

---
+++ araclar visual studyo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace araclar_visual_studyo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(710, 9);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(38, 740);
+            this.vScrollBar1.TabIndex = 0;
+            this.vScrollBar1.Tag = "";
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(757, 758);
+            this.Controls.Add(this.vScrollBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ araclar visual studyo/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace araclar_visual_studyo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (vScrollBar1.Value >= 1 && vScrollBar1.Value<=25)
+            {
+                BackColor = Color.Red;
+         
+            }
+            if(vScrollBar1.Value>=26 && vScrollBar1.Value <= 50)
+            {
+                BackColor = Color.Blue;
+            }
+            if(vScrollBar1.Value>=51&& vScrollBar1.Value <= 75)
+            {
+                BackColor = Color.Pink;
+            }
+            if (vScrollBar1.Value >= 76 && vScrollBar1.Value <= 100)
+            { 
+                BackColor = Color.Black;
+            }
+        }
+
+    }
+}
